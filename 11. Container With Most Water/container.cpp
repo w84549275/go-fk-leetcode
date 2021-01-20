9d165f6500acb2743c165a4c3b713076fa61c094
xudl <xudl@shanghaitech.edu.cn>
2021-01-20 19:13:00 +0800

    container

---
+++ 11. Container With Most Water/container.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int ans = 0;
+        int low = 0, high = height.size() - 1;
+        while (low < high){
+            if (height[low] < height[high]){
+                ans = max(ans, height[low] * (high - low));
+                low++;
+            }
+            else{
+                ans = ans = max(ans, height[high] * (high - low));
+                high--;
+            }
+        }
+        return ans;
+    }
+};
