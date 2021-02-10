981c16b021800ec0036b23d35bbdb725d028b5bd
kobeni <963423936@qq.com>
2021-02-10 18:11:47 +0800

    count all unique BSTs in DP

---
+++ 96. Unique Binary Search Trees/unique-BST.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n + 1, 0);
+        // initialization
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= i - 1; j++){
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
+};
